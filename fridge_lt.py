8d1ddd42649fbe4e2e3da72a531b99dcbeb524b0
aivarasmink <aivarasmink@gmail.com>
2024-01-03 11:05:31 +0200

    naujas

---
+++ fridge_lt.py
@@ -0,0 +1,18 @@
+""" Komandinio darbo užduotis
+===[ Šaldytuvas ]===
+
+Reikalavimai:
+
+* Šaldytuvo turinys - žodynas, kurio raktas yra produkto pavadinimas, reikšmė - kiekis (float).
+* Pridėti produktą į šaldytuvą. Pridedant egzistuojantį produktą, kiekiai sudedami su esančiais.
+* Išimti produktą iš šaldytuvo. Išimant egzistuojantį produktą, kiekis atitinkamai sumažinamas.
+* Patikrinti, ar reikiamas produkto kiekis yra šaldytuve.
+* Išspausdinti visą šaldytuvo turinį su kiekiais.
+
+BONUS:
+
+* Patikrinti, ar receptas išeina. 
+** Recepto įvedimas vyksta viena eilute, kuri po to išdalinama. Pva.: Sūris: 0.5, Pomidoras: 2, Duona: 0.4
+** Jeigu receptas neišeina, išvardinti kiek ir kokių produktų trūksta.
+
+"""
