39f326cb38c28c24d5442af799d7876dcc25a86b
Balys <you@example.combalikas4@gmail.com>
2024-01-03 14:28:39 +0200

    saldyytuvas_suvienodintas

--- saldytuvas.py
+++ saldytuvas.py
@@ -1,4 +1,7 @@
-def main():
+fridge_items = []
+# fridge_items = {produktas: kiekis}
+
+def main(): #Valdemaras
     while True:
         print('''
               asd
@@ -22,18 +25,15 @@ def main():
 
 
 def insert_item(item_name: str, item_quantity: float): #Balys
-    fridge_items = []
     item_add = {"Item name:": item_name, "Item quantity": item_quantity}
     fridge_items.append(item_add)
     print(f"{item_quantity}x{item_name} was added to the fridge")
     return fridge_items
 
-def remove_item(saldytuvas):
-    produktas = input("Įveskite produkto pavadinimą: ")
-    kiekis = float(input("Įveskite produkto kiekį: "))
-
-    if produktas in saldytuvas.item() and saldytuvas.turinys[produktas] >= kiekis:
-        produktai = {produktas: kiekis}
+def remove_item(item_name, item_quantity):    #Aivaras
+    item_remove = {"Item name:": item_name, "Item quantity": item_quantity}
+    if item_name in fridge_items.item() and fridge_items.turinys[produktas] >= kiekis:
+        
         saldytuvas.isimti_produktus(produktai)
         print(f"{produktas} išimtas iš šaldytuvo.")
     else:
@@ -41,7 +41,7 @@ def remove_item(saldytuvas):
         
       
   
-def search_item(string_entered: str, fridge_content={'None':0.00} ):
+def search_item(string_entered: str, fridge_content={'None':0.00} ):    #Maksim
     for key in fridge_content:
         if str(key).startswith(string_entered):
             print(f"Item found {key} with quantity {fridge_content[key]}!")
@@ -49,8 +49,8 @@ def search_item(string_entered: str, fridge_content={'None':0.00} ):
             print("No items found!")
     
 
-def print_items(products_list): 
-    if not products_list:
+def print_items(fridge_items): #Petras
+    if not fridge_items:
 
         print('Šaldytuvas yra tuščias. Badauk arba įdėk ką nors')
     else:
@@ -58,7 +58,6 @@ def print_items(products_list):
         for index, (item_name, item_quantity) in enumerate(fridge_items.items(), start=1):
             print(f'{index}. {item_name} : {item_quantity}')
             return
-
 
 
 """ Komandinio darbo užduotis
