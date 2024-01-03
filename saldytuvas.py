40b575f3b7e5ab3e16c90afc1013d226eb854684
vokator-online <vokator@gmail.com>
2024-01-03 13:56:29 +0200

    pakeitimai

--- saldytuvas.py
+++ saldytuvas.py
@@ -1,19 +1,24 @@
-
 def main():
-    pass
-
-def insert_item():
-    pass
-
-def remove_item():
-    pass
-
-def search_item():
-    pass
-
-def print_items():
-    pass
-
+    while True:
+        print('''
+              asd
+              asd
+              asd
+              asd
+              ''')
+        choice = input("Choice: ")
+        if choice.startswith("exit"):
+            break
+        elif choice.startswith("insert"):
+            fridge_items = insert_item(input("Item name:"), input('Item quantity:'))
+        elif choice.startswith("remove"):
+            remove_item()
+        elif choice.startswith("search"):
+            search_item()
+        elif choice.startswith("print"):
+            print_items()
+        else:
+            print("Bad choice, try again")
 
 
 
