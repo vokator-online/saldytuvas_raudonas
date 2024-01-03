d547d1348aca7bad1077d652f6f416aa1757dc68
MaxIsayev <max.i@list.ru>
2024-01-03 13:56:53 +0200

    deleted 1 file

--- Maksim_search_item.py
+++
@@ -1,6 +0,0 @@
-def search_item(string_entered: str, fridge_content={'None':0.00} ):
-    for key in fridge_content:
-        if str(key).startswith(string_entered):
-            print(f"Item found {key} with quantity {fridge_content[key]}")
-        else:
-            print("No items found")
