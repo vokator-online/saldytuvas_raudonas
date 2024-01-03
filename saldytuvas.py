d8984610e3821c9886d666693238d58b39a63ae8
MaxIsayev <max.i@list.ru>
2024-01-03 13:30:21 +0200

    search_item metodas

--- saldytuvas.py
+++ saldytuvas.py
@@ -8,8 +8,13 @@ def insert_item():
 def remove_item():
     pass
 
-def search_item():
-    pass
+def search_item(string_entered: str, fridge_content={'None':0.00} ):
+    for key in fridge_content:
+        if str(key).startswith(string_entered):
+            print(f"Item found {key} with quantity {fridge_content[key]}")
+        else:
+            print("No items found") 
+    
 
 def print_items():
     pass
