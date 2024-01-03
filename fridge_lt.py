23d3ef51de0ed54ee985f170a8855220926c1102
aivarasmink <aivarasmink@gmail.com>
2024-01-03 11:19:17 +0200

    pakeitimai

--- fridge_lt.py
+++ fridge_lt.py
@@ -16,3 +16,75 @@ BONUS:
 ** Jeigu receptas neišeina, išvardinti kiek ir kokių produktų trūksta.
 
 """
+
+class Saldytuvas:
+    def __init__(self):
+        self.turinys = {}
+
+    def prideti_produktus(self, produktai):
+        for produktas, kiekis in produktai.items():
+            if produktas in self.turinys:
+                self.turinys[produktas] += kiekis
+            else:
+                self.turinys[produktas] = kiekis
+
+    def isimti_produktus(self, produktai):
+        for produktas, kiekis in produktai.items():
+            if produktas in self.turinys:
+                self.turinys[produktas] -= kiekis
+                if self.turinys[produktas] <= 0:
+                    del self.turinys[produktas]
+            else:
+                print(f"{produktas} neegzistuoja šaldytuve.")
+
+    def tikrinti_kieki(self, produktas, reikalingas_kiekis):
+        if produktas in self.turinys and self.turinys[produktas] >= reikalingas_kiekis:
+            return True
+        else:
+            return False
+
+    def spausdinti_turini(self):
+        print("Šaldytuvo turinys:")
+        for produktas, kiekis in self.turinys.items():
+            print(f"{produktas}: {kiekis}")
+
+def patikrinti_recepta(saldytuvas, receptas):
+    recepto_produktai = dict(item.split(':') for item in receptas.split(', '))
+    truksta_produktu = {}
+
+    for produktas, reikalingas_kiekis in recepto_produktai.items():
+        reikalingas_kiekis = float(reikalingas_kiekis)
+        if not saldytuvas.tikrinti_kieki(produktas, reikalingas_kiekis):
+            truksta_produktu[produktas] = reikalingas_kiekis - saldytuvas.turinys.get(produktas, 0)
+
+    if not truksta_produktu:
+        print("Receptas išeina!")
+    else:
+        print("Receptas neišeina. Trūksta šių produktų:")
+        for produktas, trukstamas_kiekis in truksta_produktu.items():
+            print(f"{produktas}: {trukstamas_kiekis}")
+
+# Sukuriamas šaldytuvas
+saldytuvas = Saldytuvas()
+
+# Pridedami produktai į šaldytuvą
+saldytuvas.prideti_produktus({"Pienas": 2, "Kiaušiniai": 6, "Pomidoras": 4})
+
+# Išspausdinamas šaldytuvo turinys
+saldytuvas.spausdinti_turini()
+
+# Pridedami papildomi produktai
+saldytuvas.prideti_produktus({"Kiaušiniai": 4, "Sūris": 0.5, "Duona": 1})
+
+# Išspausdinamas šaldytuvo turinys
+saldytuvas.spausdinti_turini()
+
+# Išimami produktai iš šaldytuvo
+saldytuvas.isimti_produktus({"Pienas": 1, "Pomidoras": 2, "Sviestas": 0.2})
+
+# Išspausdinamas šaldytuvo turinys
+saldytuvas.spausdinti_turini()
+
+# Patikrinamas receptas
+receptas = "Sūris: 0.3, Pomidoras: 2, Duona: 0.5"
+patikrinti_recepta(saldytuvas, receptas)
