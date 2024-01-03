bd02d5e63488b5732d73b8c6b235b1336fc96c26
MaxIsayev <max.i@list.ru>
2024-01-03 13:48:27 +0200

    pakeitimai

--- Maksim_search_item.py
+++ Maksim_search_item.py
@@ -1,6 +1,6 @@
 def search_item(string_entered: str, fridge_content={'None':0.00} ):
     for key in fridge_content:
         if str(key).startswith(string_entered):
-            print(f"Item found {key} with quantity {fridge_content[key]}!")
+            print(f"Item found {key} with quantity {fridge_content[key]}")
         else:
-            print("No items found!")
+            print("No items found")
