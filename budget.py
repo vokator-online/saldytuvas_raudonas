0c684db96cf27892c0a5f8d1cb1439403e2a012f
aivarasmink <aivarasmink@gmail.com>
2024-01-04 13:02:21 +0200

    pradedu budget

---
+++ budget.py
@@ -0,0 +1,20 @@
+""" Komandinio darbo / savarankiška užduotis
+===[ Biudžetas ]===
+
+Reikalavimai
+
+* Biudžeto turinys - pajamų/išlaidų žurnalo žodynas
+** raktas - paskirtis
+** reikšmė - pajamos pozityvus float, išlaidos negatyvus float
+* Galimybė pridėti pajamas arba išlaidas
+* Spausdinti pajamų/išlaidų žurnalą
+* Suskaičiuoti biudžeto balansą
+
+"""
+
+
+budgetas = {}
+
+def main(): 
+    while True:
+        print('''===[ Biudžetas ]===''' )
