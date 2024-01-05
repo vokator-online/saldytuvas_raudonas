22dd12542ae31e5b1abf375b60968c716f42c600
aivarasmink <aivarasmink@gmail.com>
2024-01-05 10:46:32 +0200

    papyldymas dar nebaikta tik pradzia

--- Aivaro_albumai_en.py
+++ Aivaro_albumai_en.py
@@ -47,3 +47,39 @@ def list_albums():
 
 main()
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" Komandinio/individualaus darbo užduotis
+===[ Muzikos Albumas ]===
+
+Reikalavimai:
+
+* Žodynas albumas turi turėti atlikėją ir pavadinimą, gali turėti ir kitų atributų
+* Albumo žodyne sukurkite takelių (dainų) sąrašą, kur kiekvienas takelis yra žodynas, talpinantis eilės numerį, pavadinimą ir trukmę sekundėmis. 
+** Bonus: trukmės įvedimas "minutės:sekundės" formatu (žmogui suprantamu).
+* Programa turi leisti vartotojui užpildyti/pakeisti albumo informaciją (pavadinimą, atlikėją, ...)
+* Programa turi leisti vartotojui sukurti/ištrinti takelį, užpildant takelio informaciją (pavadinimą, trukmę)
+* Galimybė peržiūrėti albumą, išspausdinant takelių kiekį ir bendrą jų trukmę šalia kitų atributų.
+* Peržiūrėti albumo dainas. Bonus: išrūšiuotas pagal eilės numerį. Takelio trukmė turi būti pateikta žmogui suprantama laiko išraiška.
+
+Pastabos:
+* Stenkitės nekartoti kodo - funkcionalumui, kuriam kodas kartotųsi, parašykite atskiras funkcijas ir jas panaudokite kelis kartus kur reikia.
+"""
